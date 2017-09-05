60f48c557f4ae4e98ef74529941713f58eff7ed9
Avi Warner <aviwarner@gmail.com>
2017-09-04 21:55:38 -0700

    Assignment 8 work: modify transitions and landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,13 +2,16 @@ var animatePoints = function() {
 
   var points = document.getElementsByClassName('point');
 
-  var revealFirstPoint = function() {
-    points[0].style.opacity = 1;
-    points[0].style.transform = "scaleX(1) translateY(0)"
-    points[0].style.msTransform = "scaleX(1) translateY(0)"
-    points[0].style.WebkitTransform = "scaleX(1) translateY(0)"
+  var revealPoints = function() {
+    for (var i = 0; i < points.length; i++) {
+      points[i].style.opacity = 1;
+      points[i].style.transform = "scaleX(1) translateY(0)"
+      points[i].style.msTransform = "scaleX(1) translateY(0)"
+      points[i].style.WebkitTransform = "scaleX(1) translateY(0)"
+    }
   }
 
+/*
   var revealSecondPoint = function() {
     points[1].style.opacity = 1;
     points[1].style.transform = "scaleX(1) translateY(0)"
@@ -26,4 +29,7 @@ var animatePoints = function() {
   revealFirstPoint();
   revealSecondPoint();
   revealThirdPoint();
+  */
+
+  revealPoints();
 };
