aac3d250176b49af79a0925fbd6e7061011d5d6f
Avi Warner <aviwarner@gmail.com>
2017-09-07 10:26:41 -0700

    add plan notes to landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,15 @@
+/*
+step 0) create a utility function that accepts an array and a function then runs the function on each array value
+1) define an elements array (in this case, the array of all dom elements where the class name is 'point')
+2) create a style function that changes the css property values of an element to their final post-animation style
+3) create a animation function that inputs the elements array and the style function into the utility function
+4) call the animation function correctly when the window loads (either when the right amount of scrolling has occured or if the window is 950px high)
+*/
+
+
 var pointsArray = document.getElementsByClassName('point');
 
-function revealPoint(arrayValue) { 
+function revealPoint(arrayValue) {
   arrayValue.style.opacity = 1;
   arrayValue.style.transform = "scaleX(1) translateY(0)";
   arrayValue.style.msTransform = "scaleX(1) translateY(0)";
