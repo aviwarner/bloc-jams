265e862042b43c0a0f20e1e0a4fa43f3a8476902
Avi Warner <aviwarner@gmail.com>
2017-09-30 20:03:18 -0700

    Finished checkpoint 19 work

--- scripts/album.js
+++ scripts/album.js
@@ -1,52 +1,3 @@
-var albumPicasso = {
-  title: 'The Colors',
-  artist: 'Pablo Picasso',
-  label: 'Cubism',
-  year: '1881',
-  albumArtUrl: 'assets/images/album_covers/01.png',
-  songs: [
-    { title: 'Blue', duration: '4:26' },
-    { title: 'Green', duration: '3:14' },
-    { title: 'Red', duration: '5:01' },
-    { title: 'Pink', duration: '3:21' },
-    { title: 'Magenta', duration: '2:15' }
-  ]
-};
-var albumMarconi = {
-  title: 'The Telephone',
-  artist: 'Guglielmo Marconi',
-  label: 'EM',
-  year: '1909',
-  albumArtUrl: 'assets/images/album_covers/02.png',
-  songs: [
-    { title: 'Hello, Operator?', duration: '1:01' },
-    { title: 'Ring, ring, ring', duration: '5:01' },
-    { title: 'Fits in your pocket', duration: '5:01' },
-    { title: 'Can you hear me now?', duration: '3:14' },
-    { title: 'Wrong phone number', duration: '2:15' },
-    { title: 'Bonus Track (Live Jam)', duration: '15:42'}
-  ]
-};
-var albumPinback = {
-  title: 'This is a Pinback CD',
-  artist: 'Pinback',
-  label: 'Ace Fu Records',
-  year: '1999',
-  albumArtUrl: 'assets/images/album_covers/pinbackcover.png',
-  songs: [
-    { title: 'Tripoli', duration: '4:30' },
-    { title: 'Hurley', duration: '3:55' },
-    { title: 'Charborg', duration: '3:28' },
-    { title: 'Chaos Engine', duration: '3:38' },
-    { title: 'Shag', duration: '3:05' },
-    { title: 'Loro', duration: '3:33' },
-    { title: 'Crutch', duration: '4:31' },
-    { title: 'Rousseau', duration: '5:08' },
-    { title: 'Lyon', duration: '5:18' },
-    { title: 'Montaigne', duration: '5:38' }
-  ]
-};
-
 var createSongRow = function(songNumber, songName, songLength) {
   var template =
     '<tr class="album-view-song-item">'
@@ -58,37 +9,43 @@ var createSongRow = function(songNumber, songName, songLength) {
 
   var $row = $(template);
 
-  var clickHandler = function() {
-    var songNumber = $(this).attr('data-song-number');
-
-    if (currentlyPlayingSong === null) {
+  var clickHandler = function(songNumber) {
+    var songNumber = parseInt($(this).attr('data-song-number'));
+    if (currentlyPlayingSongNumber === null) {
       $(this).html(pauseButtonTemplate);
-      currentlyPlayingSong = songNumber;
-    } else if (currentlyPlayingSong === songNumber) {
+      currentlyPlayingSongNumber = songNumber;
+      currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+      updatePlayerBarSong(currentSongFromAlbum);
+    } else if (currentlyPlayingSongNumber === songNumber) {
       $(this).html(playButtonTemplate);
-      currentlyPlayingSong = null;
-    } else if (currentlyPlayingSong !== songNumber) {
-      var currentlyPlayingSongElement = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
-      currentlyPlayingSongElement.html(currentlyPlayingSong);
+      currentlyPlayingSongNumber = null;
+      currentSongFromAlbum = null;
+      $('.main-controls .play-pause').html(playerBarPlayButton);
+    } else {
+      var currentlyPlayingSongElement = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+      currentlyPlayingSongElement.html(currentlyPlayingSongNumber);
       $(this).html(pauseButtonTemplate);
-      currentlyPlayingSong = songNumber;
+      currentlyPlayingSongNumber = songNumber;
+      currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+      updatePlayerBarSong(currentSongFromAlbum);
     }
   };
 
   var onHover = function(event){
     var songNumberElement = $(this).find('.song-item-number');
-    var songNumber = songNumberElement.attr('data-song-number');
-    if (songNumber !== currentlyPlayingSong) {
+    var songNumber = parseInt(songNumberElement.attr('data-song-number'));
+    if (songNumber !== currentlyPlayingSongNumber) {
       songNumberElement.html(playButtonTemplate);
     }
   };
 
   var offHover = function(event){
     var songNumberElement = $(this).find('.song-item-number');
-    var songNumber = songNumberElement.attr('data-song-number');
-    if (songNumber !== currentlyPlayingSong) {
+    var songNumber = parseInt(songNumberElement.attr('data-song-number'));
+    if (songNumber !== currentlyPlayingSongNumber) {
       songNumberElement.html(songNumber);
     }
+    console.log("songNumber type is " + typeof songNumber + "\n and currentlyPlayingSongNumber type is " + typeof currentlyPlayingSongNumber);
   };
 
   $row.find('.song-item-number').click(clickHandler);
@@ -97,7 +54,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 }
 
 var setCurrentAlbum = function(album) {
-
+  currentAlbum = album;
   var $albumTitle = $('.album-view-title');
   var $albumArtist = $('.album-view-artist');
   var $albumReleaseInfo = $('.album-view-release-info');
@@ -117,11 +74,57 @@ var setCurrentAlbum = function(album) {
   }
 };
 
+var trackIndex = function(album, song) {
+  return album.songs.indexOf(song);
+};
+
+var nextSong = function() {
+  var wasPlayingNumber = trackIndex(currentAlbum, currentSongFromAlbum);
+  var nowPlayingNumber = wasPlayingNumber + 1;
+  $('.song-item-number[data-song-number="' + nowPlayingNumber + '"]').text(nowPlayingNumber);
+  if (nowPlayingNumber === currentAlbum.songs.length) {
+    nowPlayingNumber = 0;
+  }
+  $('.song-item-number[data-song-number="' + (nowPlayingNumber + 1) + '"]').html(pauseButtonTemplate);
+  currentlyPlayingSongNumber = nowPlayingNumber + 1;
+  currentSongFromAlbum = currentAlbum.songs[nowPlayingNumber];
+  updatePlayerBarSong(currentSongFromAlbum);
+};
+
+var previousSong = function() {
+  var wasPlayingNumber = trackIndex(currentAlbum, currentSongFromAlbum);
+  var nowPlayingNumber = wasPlayingNumber - 1;
+  $('.song-item-number[data-song-number="' + (wasPlayingNumber + 1) + '"]').text(wasPlayingNumber + 1);
+  if (wasPlayingNumber === 0) {
+    nowPlayingNumber = currentAlbum.songs.length - 1;
+  }
+  $('.song-item-number[data-song-number="' + (nowPlayingNumber + 1) + '"]').html(pauseButtonTemplate);
+  currentSongFromAlbum = currentAlbum.songs[nowPlayingNumber];
+  updatePlayerBarSong(currentSongFromAlbum);
+};
+
+var updatePlayerBarSong = function(song) {
+    $('.artist-name').text(currentAlbum.artist);
+    $('.song-name').text(song.title);
+    $('.artist-song-mobile').text(currentAlbum.artist + ' - ' + song.title);
+    $('.total-time').text(song.duration);
+    $('.current-time').text('0:00');
+    $('.main-controls .play-pause').html(playerBarPauseButtom);
+};
+
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+var playerBarPlayButton = '<span class="ion-play"></span>';
+var playerBarPauseButtom = '<span class="ion-pause"></span>';
 
-var currentlyPlayingSong = null;
+var currentAlbum = null;
+var currentlyPlayingSongNumber = null;
+var currentlySongFromAlbum = null;
+var $previousButton = $('.main-controls .previous');
+var $nextButton = $('.main-controls .next');
 
 $(document).ready(function(){
   setCurrentAlbum(albumPinback);
+  $previousButton.click(previousSong);
+  $nextButton.click(nextSong);
 });
---
+++ scripts/fixtures.js
@@ -0,0 +1,48 @@
+var albumPicasso = {
+  title: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  albumArtUrl: 'assets/images/album_covers/01.png',
+  songs: [
+    { title: 'Blue', duration: '4:26' },
+    { title: 'Green', duration: '3:14' },
+    { title: 'Red', duration: '5:01' },
+    { title: 'Pink', duration: '3:21' },
+    { title: 'Magenta', duration: '2:15' }
+  ]
+};
+var albumMarconi = {
+  title: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  label: 'EM',
+  year: '1909',
+  albumArtUrl: 'assets/images/album_covers/02.png',
+  songs: [
+    { title: 'Hello, Operator?', duration: '1:01' },
+    { title: 'Ring, ring, ring', duration: '5:01' },
+    { title: 'Fits in your pocket', duration: '5:01' },
+    { title: 'Can you hear me now?', duration: '3:14' },
+    { title: 'Wrong phone number', duration: '2:15' },
+    { title: 'Bonus Track (Live Jam)', duration: '15:42'}
+  ]
+};
+var albumPinback = {
+  title: 'This is a Pinback CD',
+  artist: 'Pinback',
+  label: 'Ace Fu Records',
+  year: '1999',
+  albumArtUrl: 'assets/images/album_covers/pinbackcover.png',
+  songs: [
+    { title: 'Tripoli', duration: '4:30' },
+    { title: 'Hurley', duration: '3:55' },
+    { title: 'Charborg', duration: '3:28' },
+    { title: 'Chaos Engine', duration: '3:38' },
+    { title: 'Shag', duration: '3:05' },
+    { title: 'Loro', duration: '3:33' },
+    { title: 'Crutch', duration: '4:31' },
+    { title: 'Rousseau', duration: '5:08' },
+    { title: 'Lyon', duration: '5:18' },
+    { title: 'Montaigne', duration: '5:38' }
+  ]
+};
